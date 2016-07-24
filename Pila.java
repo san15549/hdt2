6697f5a99e054c12175f754981470c1ac79e753d
san15549 <san15549@uvg.edu.gt>
2016-07-24 14:49:11 -0600

    Create Pila.java

---
+++ Pila.java
@@ -0,0 +1,37 @@
+/**
+ * @author Carlo David Sanchinelli, Diego Alberto Hurtarte
+ * @version 24.07.16
+ */
+public class Pila implements iPila {
+    private String[] stack;
+    
+    public Pila(){
+        stack = new String[] {"null","null","null","null","null","null","null","null","null","null"};
+    }
+//------------------------------------------------------------------------------
+
+    @Override
+    public void Push(String object) {
+        int i=0;
+        while(!stack[i].equals("null")){
+            i=i+1;
+        }
+        stack[i] = object;
+    }
+
+    @Override
+    public String Pop() {
+        int i=9;
+        while(stack[i].equals("null")){
+            i=i-1;
+        }
+        String object = stack[i];
+        stack[1] = "null";
+        return object;
+    }
+
+    @Override
+    public String toString() {
+        return "Pila{" + "stack=" + stack + '}';
+    }
+}
